2ce081ba72a041a6d99c6e8de266057fd9107a65
Nina <>
2021-10-06 15:58:09 +0200

    basic styling, hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/hero.css';
+
+export default function Hero() {
+  return (
+    <div className="content"> 
+      <img id="hero-img" src="https://enfo-test-resources.s3.eu-west-2.amazonaws.com/images/end-user-remote-work5.jpg" alt="woman on computer with headphones on"/>
+
+            <div class="title">
+                <div class="enfo">Enfo</div>
+                Employee list
+            </div>
+    </div>
+  )
+}
