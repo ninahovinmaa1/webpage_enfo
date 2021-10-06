704b07f5b40f9c89e364678f5478c24263abd0a0
Nina <>
2021-10-06 16:28:53 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Table from './components/Table';
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <Table />
         <ThanksLinks />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <p>Privacy Policy  @ 2021 Enfo. All rights reserved.</p>
+    </>
+  )
+}
